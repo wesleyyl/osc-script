5930153dc7849c4967895dbf1494a257dd74cdfb
WesleyyL <wesleyluk.111@gmail.com>
2023-04-01 19:36:48 -0700

    simplification instructions

--- modelSimplifier.py
+++ modelSimplifier.py
@@ -1,6 +1,9 @@
 #first start by adding all the tellurium stuff around it!
-#a way to reduce rate constant values (round k to smaller and smaller values)
+#a way to reduce rate constant values by truncation (round k to smaller and smaller values)
 #commenting out one line at a time to see if it still remains an oscillator
+    #comment out and then comment back -> annotate with important, non-essential, and CVODE error
+    #then try different combinations of commenting out the non-essential ones
 #look for autocatalytic reactions!!
 #remove variables that don't affect reaction
 	#can do this by commenting out every line with a certain variable and seeing if it still oscillates
+#use pathway designer to draw out the reaction
