b73a0aff7d104003cb1c34a54da810f1ba2c99cd
WesleyyL <wesleyluk.111@gmail.com>
2023-04-01 20:33:04 -0700

    include config details in extract

--- unzipAntimony.py
+++ unzipAntimony.py
@@ -18,7 +18,7 @@ if not os.path.exists(extractedAnt):
 def readAntStr(zipFile):
     with ZipFile(zipFile) as zipF:
         with zipF.open('best_antimony.ant') as ant:
-            return ant.read()
+            return ant.read().decode('utf-8')
 
 def readSeedStr(zipFile):
     with ZipFile(zipFile) as zipF:
@@ -30,6 +30,11 @@ def readSeedStr(zipFile):
             seed_str = seed.read().decode('utf-8')
             #print(seed_str)
             return seed_str
+
+def readConfig(zipFile):
+    with ZipFile(zipFile) as zipF:
+        with zipF.open('config.txt') as cfg:
+            return cfg.read().decode('utf-8')
 
 
 fileList = [i for i in (os.path.join(savedModels, f) for f in os.listdir(savedModels)) if os.path.isfile(i)]
@@ -45,8 +50,16 @@ for zipFile in fileList:
     seed = str(seedNum) + ".txt"
     seedPath = os.path.join(extractedAnt, seed)
     
-    with open(seedPath, 'wb') as file:
+    with open(seedPath, 'w') as file:
         antStr = readAntStr(filepath)
-        file.write(('Seed: ' + str(seedNum)+ '\n\n').encode())
+        modelConfig = readConfig(filepath)
+        file.write('Seed: ' + str(seedNum)+ '\n\n')
         file.write(antStr)
+        file.write('\n\n\n')
+        file.write(modelConfig)
+
+    # with open(seedPath, 'wb') as file:
+    #     antStr = readAntStr(filepath)
+    #     file.write(('Seed: ' + str(seedNum)+ '\n\n').encode())
+    #     file.write(antStr)
 
